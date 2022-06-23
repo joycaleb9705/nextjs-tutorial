6eeed93831c6d4dd536d56d9b287f6e65853cc19
joycaleb9705 <joycaleb9705@berkeley.edu>
2022-06-22 20:00:16 -0700

    first commit

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import NavBar from "../components/NavBar";
+import Title from "../components/Title";
+
+export default function About() {
+    return (
+        <div>
+            <Title title="About"/>
+            About ME
+        </div>
+        
+    );
+}
